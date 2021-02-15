447ed5af83573f409e144114ada2598de59a2c4d
Spiderjockey2 <benjamin.forey11gmail.com>
2021-02-15 22:40:37 +0000

    updated plugin

--- dist/plugin.js
+++ dist/plugin.js
@@ -20,22 +20,17 @@ const __awaiter = (this && this.__awaiter) || function(thisArg, _arguments, P, g
 		step((generator = generator.apply(thisArg, _arguments || [])).next());
 	});
 };
-const __importDefault = (this && this.__importDefault) || function(mod) {
-	return (mod && mod.__esModule) ? mod : { 'default': mod };
-};
 
 Object.defineProperty(exports, '__esModule', { value: true });
 exports.Facebook = void 0;
 const erela_js_1 = require('erela.js');
-const axios_1 = __importDefault(require('axios'));
 const REGEX = /(https?:\/\/)(www\.|m\.)?(facebook|fb).com\/.*\/videos\/.*/;
 const fetch = require('node-fetch').default;
 const { JSDOM } = require('jsdom');
 
 const check = (options) => {
-    if (typeof options.convertUnresolved !== "undefined" &&
-        typeof options.convertUnresolved !== "boolean")
-        throw new TypeError('Spotify option "convertUnresolved" must be a boolean.');
+	if (typeof options.convertUnresolved !== 'undefined' &&
+        typeof options.convertUnresolved !== 'boolean') {throw new TypeError('Spotify option "convertUnresolved" must be a boolean.');}
 
 };
 
@@ -50,21 +45,20 @@ const buildSearch = (loadType, tracks, error) => ({
 });
 
 class Facebook extends erela_js_1.Plugin {
-	constructor(options) {
+	constructor(options = {}) {
 		super();
 		check(options);
-    this.options = Object.assign({}, options);
-		this.functions = {
-			track: this.getTrack.bind(this),
-		};
+		this.options = Object.assign({}, options);
 	}
 
+	// load up the plugin
 	load(manager) {
 		this.manager = manager;
 		this._search = manager.search.bind(manager);
 		manager.search = this.search.bind(this);
 	}
 
+	// search up the facebook video
 	search(query, requester) {
 		let _a, _b, _c;
 		return __awaiter(this, void 0, void 0, function* () {
@@ -88,8 +82,10 @@ class Facebook extends erela_js_1.Plugin {
 						data.tracks[0].title = obj.title;
 						data.tracks[0].thumbnail = obj.thumbnail;
 						data.tracks[0].uri = obj.url;
-						// if (this.options.convertUnresolved) {track.resolve();}
-						return buildSearch('TRACK_LOADED', [data.tracks], null);
+						if (this.options.convertUnresolved) {
+							data.resolve();
+						}
+						return buildSearch('TRACK_LOADED', data.tracks, null);
 					} else {
 						const msg = 'Incorrect type for Facebook URL.';
 						return buildSearch('LOAD_FAILED', null, msg);
@@ -100,31 +96,6 @@ class Facebook extends erela_js_1.Plugin {
 				}
 			}
 			return this._search(query, requester);
-		});
-	}
-
-	getTrack(id) {
-		console.log('dfhjldfshj');
-		return __awaiter(this, void 0, void 0, function* () {
-			try {
-				const html = yield fetch(id.replace('/m.', '/')).text();
-				const document = new JSDOM(html).window.document;
-				const rawdata = document.querySelector('script[type="application/ld+json"]').innerHTML;
-				const json = JSON.parse(rawdata);
-				const obj = {
-					title: document.querySelector('meta[property="og:title"]').attributes.item(1).value,
-					thumbnail: json.thumbnailUrl,
-					streamURL: json.url,
-					url: html.split('",page_uri:"')[1].split('",')[0],
-					author: json.author.name,
-				};
-				const track = obj.streamURL;// Facebook.convertToUnresolved(json.url);
-				console.log('Hello' + track);
-				return { tracks: [track] };
-			} catch (e) {
-				console.log(e);
-				return { tracks: null };
-			}
 		});
 	}
 }
