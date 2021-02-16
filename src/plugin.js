951bd46c5cd273f035ea7289fe5ec80449912a5c
Spiderjockey2 <benjamin.forey11gmail.com>
2021-02-16 13:46:09 +0000

    added src folder + commands

---
+++ src/plugin.js
@@ -0,0 +1,102 @@
+'use strict';
+const __awaiter = (this && this.__awaiter) || function(thisArg, _arguments, P, generator) {
+	function adopt(value) {
+		return value instanceof P ? value : new P(function(resolve) {
+			resolve(value);
+		});
+	}
+	return new (P || (P = Promise))(function(resolve, reject) {
+		function fulfilled(value) {
+			try {
+				step(generator.next(value));
+			} catch (e) { reject(e); }
+		}
+		function rejected(value) {
+			try {
+				step(generator['throw'](value));
+			} catch (e) { reject(e); }
+		}
+		function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+		step((generator = generator.apply(thisArg, _arguments || [])).next());
+	});
+};
+
+Object.defineProperty(exports, '__esModule', { value: true });
+exports.Facebook = void 0;
+const erela_js_1 = require('erela.js');
+const REGEX = /(https?:\/\/)(www\.|m\.)?(facebook|fb).com\/.*\/videos\/.*/;
+const fetch = require('node-fetch').default;
+const { JSDOM } = require('jsdom');
+
+const check = (options) => {
+	if (typeof options.convertUnresolved !== 'undefined' &&
+        typeof options.convertUnresolved !== 'boolean') {throw new TypeError('Spotify option "convertUnresolved" must be a boolean.');}
+
+};
+
+const buildSearch = (loadType, tracks, error) => ({
+	loadType: loadType,
+	tracks: tracks !== null && tracks !== void 0 ? tracks : [],
+	playlist: null,
+	exception: error ? {
+		message: error,
+		severity: 'COMMON',
+	} : null,
+});
+
+class Facebook extends erela_js_1.Plugin {
+	constructor(options = {}) {
+		super();
+		check(options);
+		this.options = Object.assign({}, options);
+	}
+
+	// load up the plugin
+	load(manager) {
+		this.manager = manager;
+		this._search = manager.search.bind(manager);
+		manager.search = this.search.bind(this);
+	}
+
+	// search up the facebook video
+	search(query, requester) {
+		let _a, _b, _c;
+		return __awaiter(this, void 0, void 0, function* () {
+			const finalQuery = query.query || query;
+			const [, type, id] = (_a = finalQuery.match(REGEX)) !== null && _a !== void 0 ? _a : [];
+			if (finalQuery.match(REGEX)) {
+				try {
+					const html = yield fetch(query.replace('/m.', '/')).then(res => res.text());
+					const document = new JSDOM(html).window.document;
+					const rawdata = document.querySelector('script[type="application/ld+json"]').innerHTML;
+					const json = JSON.parse(rawdata);
+					const obj = {
+						title: document.querySelector('meta[property="og:title"]').attributes.item(1).value,
+						thumbnail: json.thumbnailUrl,
+						streamURL: json.url,
+						url: html.split('",page_uri:"')[1].split('",')[0],
+						author: json.author.name,
+					};
+					if (obj.streamURL) {
+						const data = yield this.manager.search(obj.streamURL, requester);
+						data.tracks[0].title = obj.title;
+						data.tracks[0].thumbnail = obj.thumbnail;
+						data.tracks[0].uri = obj.url;
+						if (this.options.convertUnresolved) {
+							data.resolve();
+						}
+						return buildSearch('TRACK_LOADED', data.tracks, null);
+					} else {
+						const msg = 'Incorrect type for Facebook URL.';
+						return buildSearch('LOAD_FAILED', null, msg);
+					}
+				} catch (e) {
+					console.log(e);
+					return buildSearch((_b = e.loadType) !== null && _b !== void 0 ? _b : 'LOAD_FAILED', null, (_c = e.message) !== null && _c !== void 0 ? _c : null, null);
+				}
+			}
+			return this._search(query, requester);
+		});
+	}
+}
+exports.Facebook = Facebook;
---
+++ src/plugin.ts
@@ -0,0 +1,129 @@
+import {
+  Manager,
+  Plugin,
+  TrackUtils,
+  UnresolvedTrack,
+  UnresolvedQuery,
+  LoadType,
+  SearchQuery,
+} from "erela.js";
+
+const REGEX = /(https?:\/\/)(www\.|m\.)?(facebook|fb).com\/.*\/videos\/.*/;
+const fetch = require("node-fetch").default;
+const { JSDOM } = require("jsdom");
+
+const check = (options: Options) => {
+  if (!options) throw new TypeError("Options must not be empty.");
+  if (
+    typeof options.convertUnresolved !== "undefined" &&
+    typeof options.convertUnresolved !== "boolean"
+  )
+    throw new TypeError(' option "convertUnresolved" must be a boolean.');
+};
+
+const buildSearch = (
+  loadType: LoadType,
+  tracks: UnresolvedTrack[],
+  error: string
+): SearchResult => ({
+  loadType: loadType,
+  tracks: tracks ?? [],
+  playlist: null,
+  exception: error
+    ? {
+        message: error,
+        severity: "COMMON",
+      }
+    : null,
+});
+
+export class Facebook extends Plugin {
+  private _search: (
+    query: string | SearchQuery,
+    requester?: unknown
+  ) => Promise<SearchResult>;
+  private manager: Manager;
+  private readonly options: Options;
+
+  public constructor(options: Options) {
+    super();
+    check(options);
+    this.options = {
+      ...options,
+    };
+  }
+
+  public load(manager: Manager) {
+    this.manager = manager;
+    this._search = manager.search.bind(manager);
+    manager.search = this.search.bind(this);
+  }
+
+  private async search(
+    query: string | SearchQuery,
+    requester?: unknown
+  ): Promise<SearchResult> {
+    const finalQuery = (query as SearchQuery).query || (query as string);
+    if (finalQuery.match(REGEX)) {
+      try {
+        const html = fetch(query.replace("/m.", "/")).then((res) => res.text());
+        const document = new JSDOM(html).window.document;
+        const rawdata = document.querySelector('script[type="application/ld+json"]').innerHTML;
+        const json = JSON.parse(rawdata);
+        const obj = {
+          title: document.querySelector('meta[property="og:title"]').attributes.item(1).value,
+          thumbnail: json.thumbnailUrl,
+          streamURL: json.url,
+          url: html.split('",page_uri:"')[1].split('",')[0],
+          author: json.author.name,
+        };
+        if (obj.streamURL) {
+          const data = this.manager.search(obj.streamURL, requester);
+          data.tracks[0].title = obj.title;
+          data.tracks[0].thumbnail = obj.thumbnail;
+          data.tracks[0].uri = obj.url;
+          if (this.options.convertUnresolved) {
+            data.resolve();
+          }
+          return buildSearch("TRACK_LOADED", data.tracks, null);
+        } else {
+          const msg = "Incorrect type for Facebook URL.";
+          return buildSearch("LOAD_FAILED", null, msg);
+        }
+      } catch (e) {
+        return buildSearch(
+          e.loadType ?? "LOAD_FAILED",
+          null,
+          e.message ?? null
+        );
+      }
+    }
+    return this._search(query, requester);
+  }
+}
+
+export interface Result {
+  tracks: UnresolvedQuery[];
+  name?: string;
+}
+
+export interface Options {
+  /**
+   * Whether to convert UnresolvedTracks to Track. Defaults to false.
+   * **Note: This is** ***not*** **recommended as it spams YouTube and takes a while if a large playlist is loaded.**
+   */
+  convertUnresolved?: boolean;
+}
+
+export interface SearchResult {
+  exception?: {
+    severity: string;
+    message: string;
+  };
+  loadType: string;
+  playlist?: {
+    duration: number;
+    name: string;
+  };
+  tracks: UnresolvedTrack[];
+}
